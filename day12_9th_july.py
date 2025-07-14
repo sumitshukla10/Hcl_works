09c28958d50bbcd9a39e7227d1e4a4a696b625eb
Sumit Shukla <sumit.shu@hcltech.com>
2025-07-14 13:18:34 +0530

    added pratice probelms

--- day12_9th_july.py
+++ day12_9th_july.py
@@ -10,3 +10,13 @@ def print_numbers():
 print_numbers()
 
 # 2. Write a program using threading to simulate downloading two files simultaneously. Show how Python can multitask using threads.
+
+def function_to_download_file(file_name):
+    print(f"file downloading starts : {file_name}")
+
+    for i in range(1,6):
+        print(f"Downloading ffile {file_name} : {i*20}%")
+    print(f"Downloading finished : {file_name}")
+
+function_to_download_file("file1")
+function_to_download_file("file2")
